458094e2999c31ca034dc5a03d61d8abdc558727
Ralph Moon <ralph.moon@comcast.net>
2024-11-27 11:09:01 -0800

    Added DirFrom()

--- directory/directory.go
+++ directory/directory.go
@@ -1,6 +1,12 @@
 package directory
 
-import ""github.com/rhm-omm/marc/ldr""
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/rhm-omm/marc/ldr"
+)
 
 type entry struct {
 	tag      int
@@ -13,11 +19,36 @@ type Directory []entry
 func DirFrom(MARCrec []byte) Directory {
 	// Get base address from leader
 	l := ldr.LdrFrom(MARCrec)
+	baseAddr := l.BaseAddr()
 	// Create the slice that defines the directory
+	dirSrc := MARCrec[24:baseAddr] // Includes field terminator
 	// Create Directory vbl
+	var dir []entry
 	// Loop through the bytes, filling out the entry struct
-	// Append entry to directory, and repeat
-	// Return directory
+	for i := 0; i < baseAddr; i += 12 {
+		var e entry
+		t, err := strconv.Atoi(string(dirSrc[i : i+3]))
+		if err != nil {
+			fmt.Println("Tag not a number")
+			os.Exit(1)
+		}
+		e.tag = t
+		l, err := strconv.Atoi(string(dirSrc[i+3 : i+7]))
+		if err != nil {
+			fmt.Println("Length not a number")
+			os.Exit(1)
+		}
+		e.fldlen = l
+		s, err := strconv.Atoi(string(dirSrc[i+7 : i+12]))
+		if err != nil {
+			fmt.Println("Starting position not a number")
+			os.Exit(1)
+		}
+		e.startpos = s
+		// Append entry to directory, and repeat
+		dir = append(dir, e)
+	}
+	return dir
 }
 
 var fldMap = make(map[int]entry)
