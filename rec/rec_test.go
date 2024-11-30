f609cb7590122ea5bee9369a167181baae214f9b
Ralph Moon <ralph.moon@comcast.net>
2024-11-30 11:02:09 -0800

    rec_test

--- rec/rec_test.go
+++ rec/rec_test.go
@@ -5,15 +5,22 @@ import (
 	"os"
 	"testing"
 
-	"github.com/rhm-omm/marc/rec"
+	"github.com/rhm-omm/marc/ldr"
 )
 
-var fname = "c:\\users\\rhmoo\\OneDrive\\Documents\\RMoon.mrc"
+const fname = "c:\\users\\rhmoo\\OneDrive\\Documents\\RMoon.mrc"
 
 func TestNext(t *testing.T) {
 	var f, err = os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
 	}
-	r := rec.Next(f, 0)
+
+	r := Next(f, 0)
+	l := ldr.LdrFrom(r)
+	recLen := l.RecLen()
+	if recLen != 4108 {
+		t.Errorf("4108 expected, got %d", recLen)
+	}
+
 }
