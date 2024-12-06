9c602bad785132989bb96ed0c1da8f0f44c1103c
Ralph Moon <ralph.moon@comcast.net>
2024-12-06 11:58:47 -0800

    Convert []byte to []rune via string

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/rhm-omm/marc/ctrlfld"
 )
 
 const fname = "c:\\users\\rhmoo\\OneDrive\\Documents\\RMoon.mrc"
@@ -16,13 +14,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	i := bytes.IndexByte(data, 0x1d) // Locate first record terminator (RT)
 	r := data[:i]                    // First record, w/o RT
-
-	cfa := ctrlfld.CtrlFldFrom(r)
-	for _, cf := range cfa {
-		fmt.Print(cf.TagOf(), ": ")
-		lv := len(cf.ValueOf())
-		fmt.Println(cf.ValueOf(), "(", lv, ")")
-	}
+	s := string(r)
+	ra := []rune(s)
+	fmt.Printf("Length of byte array = %d\n", len(r))
+	fmt.Printf("Length of string = %d\n", len(s))
+	fmt.Printf("Length of rune array = %d\n", len(ra))
 }
